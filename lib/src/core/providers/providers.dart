266df19ffde2350e3aa6ab0d88797ed54651ae7a
Garrett Hayes <garrettbridgeshayes@gmail.com>
2025-02-27 20:32:07 -0600

    hoopla

--- lib/src/core/providers/providers.dart
+++ lib/src/core/providers/providers.dart
@@ -18,7 +18,7 @@ part 'providers.g.dart';
 @riverpod
 Dio dio(Ref ref) {
   return Dio(BaseOptions(
-    baseUrl: 'http://192.168.1.221:3000',  // Replace with your API URL
+    baseUrl: 'http://yourtourserv.duckdns.org:3000',  // Replace with your API URL
     contentType: 'application/json',
   ));
 }
